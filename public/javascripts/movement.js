b841156231a52b50aa43dc8ed737d3aea4fa57de
frederico <thecanuckatheist@gmail.com>
2021-01-23 22:14:10 -0500

    added shot animation

--- public/javascripts/movement.js
+++ public/javascripts/movement.js
@@ -1,23 +1,45 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+
   function moveSquare(e) {
       const pixel = document.getElementById("starting-pixel");
       const game = document.getElementById("game");
 
       const up = window.getComputedStyle(pixel, null).top;
+      const left = window.getComputedStyle(pixel, null).left;
+      
+      console.log(up,left);
+
+      let newUp = up.replace("px", "");
+      let numUp = parseInt(newUp);
+
+      let newLeft = left.replace("px", "");
+      let numLeft = parseInt(newLeft);
 
       switch(e.which) {
           case 32:
               console.log("Space Bar");
+              let shotUp = numUp - 20;
+              let shotLeft = numLeft + 13;
               const shot = document.createElement("div");
               shot.setAttribute("id", "shot");
-              shot.style.left = "100px";
-              shot.style.top = "100px";
+              shot.style.left = shotLeft + "px";
+              shot.style.top = shotUp + "px";
               game.appendChild(shot);
+
+              let newShotUp = shotUp - 1;
+
+              let timer = setInterval(() => {
+                document.getElementById("shot").style.top = newShotUp + "px";
+                newShotUp--;
+                if (newShotUp < 0) {
+                    clearInterval(timer);
+                    shot.parentNode.removeChild(shot);
+                  }
+              }, 5);
+              
               break
           case 38:
               console.log("Arrow Up");
-              let newUp = up.replace("px", "");
-              let numUp = parseInt(newUp);
               let newNumUp = numUp - 5;
               pixel.style.top = "" + newNumUp + "px";
               console.log(up);
