be533ea90089589a4d784fce18ab14d7b9062f24
frederico <thecanuckatheist@gmail.com>
2021-01-28 22:05:16 -0500

    building logic to detect when bullet hits enemy, not working

--- public/javascripts/altMovement.js
+++ public/javascripts/altMovement.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
   const press = ['32', '37', '38', '39', '40'];
   let shotCount = 0;  //for creating unique IDs for each bullet element
 
+  let enemyBottom = []; // area to check if the bullet hits
+
   const pixel = {
     '38': 0,  //arrow-up
     '40': 0,  //arrow-down
@@ -18,6 +20,10 @@ document.addEventListener('DOMContentLoaded', ()=> {
   function keys(e) {
     if (!press.includes(e.which + '')) {return;}
     pixel[e.which + ''] = (event.type === 'keyup') ? 0 : 1;
+
+    if (pixel['32'] === 1) {
+      bullet();
+    }
   }
 
   function bullet(){
@@ -35,6 +41,16 @@ document.addEventListener('DOMContentLoaded', ()=> {
     let newShotUp = shotUp - 1;
 
     let timer = setInterval(() => {
+
+      for (let j = shotLeft; j < shotLeft + 6; j++) {
+        if (enemyBottom.includes([newShotUp, j])) {
+          clearInterval(timer);
+          shot.parentNode.removeChild(shot);
+          let dude = document.getElementsByClassName("enemy");
+          dude.parentNode.removeChild(dude);
+        }
+      }
+
       document.getElementById(shotString).style.top = newShotUp + "px";
       newShotUp--;
       if (newShotUp < 0) {
@@ -47,17 +63,35 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
   }
 
+  function enePOS(){
+    let eneY = 100;
+    let eneX = 400;
+    const mob = document.createElement("div");
+    mob.setAttribute('class', 'enemy');
+    mob.style.left = eneX + 'px';
+    mob.style.top = eneY + 'px';
+    game.appendChild(mob);
+
+    let eneYBottom = eneY + 32 - 1;
+    let eneXBottom = eneX + 32 - 1;
+    for (let i = eneX; i <= eneXBottom; i++) {
+      enemyBottom.push([eneYBottom, i]);
+    }
+    console.log(enemyBottom);
+  }
+
+
+
   function anime() {
     const dirX = pixel['37'] * -pixel.speed + pixel['39'] * pixel.speed;
     const dirY = pixel['38'] * -pixel.speed + pixel['40'] * pixel.speed;
     pixel.posX += dirX;
     pixel.posY += dirY;
     pixel.el.style.transform = `translate(${pixel.posX}px, ${pixel.posY}px)`;
-    if (pixel['32'] === 1) {
-      bullet();
-    }
     requestAnimationFrame(anime);
   }
+
+  enePOS();
 
   document.addEventListener('keyup', keys);
   document.addEventListener('keydown', keys);
